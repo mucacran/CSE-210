ebdfd7c1e87c81cd4985fc92daf92b2c099a93c0
mucacran <el.mucacran.rasta@hotmail.com>
2024-10-17 20:52:58 -0500

    actualizacion

---
+++ WEEK5/Fraction/Fraction.cs
@@ -0,0 +1,19 @@
+class Fraction{
+    private _top;
+    private _bottom;
+
+    public Fraction(){
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int top){
+        _top = top;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom){
+        _top = top;
+        _bottom = buttom;
+    }
+}
